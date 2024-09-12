e89991da76d2f243e9899b3d0a8e61e38f164e9e
Johannes Rauch <johannes.rauch@actindo.com>
2024-09-12 09:14:50 +0200

    split stack WIP

--- src/SplitStack.cpp
+++ src/SplitStack.cpp
@@ -15,6 +15,8 @@ struct SplitStack : Module {
 	enum InputId {
 		VOCT_INPUT,
 		GATE_INPUT,
+		VEL_INPUT,
+		AFT_INPUT,
 		RETRIG_INPUT,
 		INPUTS_LEN
 	};
@@ -23,6 +25,10 @@ struct SplitStack : Module {
 		VOCT_B_OUTPUT,
 		GATE_A_OUTPUT,
 		GATE_B_OUTPUT,
+		VEL_A_OUTPUT,
+		VEL_B_OUTPUT,
+		AFT_A_OUTPUT,
+		AFT_B_OUTPUT,
 		RETRIG_A_OUTPUT,
 		RETRIG_B_OUTPUT,
 		OUTPUTS_LEN
@@ -47,11 +53,17 @@ struct SplitStack : Module {
 		configSwitch(SWITCH_PARAM, 0.f, 1.f, 0.f, "Switch A↔B", {"off", "on"});
 		configInput(VOCT_INPUT, "V/Oct");
 		configInput(GATE_INPUT, "Gate");
+		configInput(VEL_INPUT, "Velocity");
+		configInput(AFT_INPUT, "Aftertouch");
 		configInput(RETRIG_INPUT, "Retrigger");
 		configOutput(VOCT_A_OUTPUT, "V/Oct A");
 		configOutput(VOCT_B_OUTPUT, "V/Oct B");
 		configOutput(GATE_A_OUTPUT, "Gate A");
 		configOutput(GATE_B_OUTPUT, "Gate B");
+		configOutput(VEL_A_OUTPUT, "Velocity A");
+		configOutput(VEL_B_OUTPUT, "Velocity B");
+		configOutput(AFT_A_OUTPUT, "Aftertouch A");
+		configOutput(AFT_B_OUTPUT, "Aftertouch B");
 		configOutput(RETRIG_A_OUTPUT, "Retrigger A");
 		configOutput(RETRIG_B_OUTPUT, "Retrigger B");
 	}
@@ -78,6 +90,16 @@ struct SplitStack : Module {
 				outputs[GATE_A_OUTPUT].setVoltageSimd(inputs[GATE_INPUT].getPolyVoltageSimd<float_4>(c), c);
 				outputs[GATE_B_OUTPUT].channels = channels;
 				outputs[GATE_B_OUTPUT].setVoltageSimd(inputs[GATE_INPUT].getPolyVoltageSimd<float_4>(c), c);
+
+				outputs[VEL_A_OUTPUT].channels = channels;
+				outputs[VEL_A_OUTPUT].setVoltageSimd(inputs[VEL_INPUT].getPolyVoltageSimd<float_4>(c), c);
+				outputs[VEL_B_OUTPUT].channels = channels;
+				outputs[VEL_B_OUTPUT].setVoltageSimd(inputs[VEL_INPUT].getPolyVoltageSimd<float_4>(c), c);
+
+				outputs[AFT_A_OUTPUT].channels = channels;
+				outputs[AFT_A_OUTPUT].setVoltageSimd(inputs[AFT_INPUT].getPolyVoltageSimd<float_4>(c), c);
+				outputs[AFT_B_OUTPUT].channels = channels;
+				outputs[AFT_B_OUTPUT].setVoltageSimd(inputs[AFT_INPUT].getPolyVoltageSimd<float_4>(c), c);
 
 				outputs[RETRIG_A_OUTPUT].channels = channels;
 				outputs[RETRIG_A_OUTPUT].setVoltageSimd(inputs[RETRIG_INPUT].getPolyVoltageSimd<float_4>(c), c);
@@ -147,6 +169,16 @@ struct SplitStack : Module {
 				outputs[GATE_B_OUTPUT].channels = channels;
 				outputs[GATE_B_OUTPUT].setVoltageSimd(~mask & inputs[GATE_INPUT].getPolyVoltageSimd<float_4>(c), c);
 
+				outputs[VEL_A_OUTPUT].channels = channels;
+				outputs[VEL_A_OUTPUT].setVoltageSimd(mask & inputs[VEL_INPUT].getPolyVoltageSimd<float_4>(c), c);
+				outputs[VEL_B_OUTPUT].channels = channels;
+				outputs[VEL_B_OUTPUT].setVoltageSimd(~mask & inputs[VEL_INPUT].getPolyVoltageSimd<float_4>(c), c);
+
+				outputs[AFT_A_OUTPUT].channels = channels;
+				outputs[AFT_A_OUTPUT].setVoltageSimd(mask & inputs[AFT_INPUT].getPolyVoltageSimd<float_4>(c), c);
+				outputs[AFT_B_OUTPUT].channels = channels;
+				outputs[AFT_B_OUTPUT].setVoltageSimd(~mask & inputs[AFT_INPUT].getPolyVoltageSimd<float_4>(c), c);
+
 				outputs[RETRIG_A_OUTPUT].channels = channels;
 				outputs[RETRIG_A_OUTPUT].setVoltageSimd(mask & inputs[RETRIG_INPUT].getPolyVoltageSimd<float_4>(c), c);
 				outputs[RETRIG_B_OUTPUT].channels = channels;
@@ -173,6 +205,14 @@ struct SplitStack : Module {
 					outputs[GATE_B_OUTPUT].channels = channels;
 					outputs[GATE_B_OUTPUT].setVoltageSimd(inputs[GATE_INPUT].getPolyVoltageSimd<float_4>(c), c);
 
+					outputs[VEL_A_OUTPUT].channels = 0;
+					outputs[VEL_B_OUTPUT].channels = channels;
+					outputs[VEL_B_OUTPUT].setVoltageSimd(inputs[VEL_INPUT].getPolyVoltageSimd<float_4>(c), c);
+
+					outputs[AFT_A_OUTPUT].channels = 0;
+					outputs[AFT_B_OUTPUT].channels = channels;
+					outputs[AFT_B_OUTPUT].setVoltageSimd(inputs[AFT_INPUT].getPolyVoltageSimd<float_4>(c), c);
+
 					outputs[RETRIG_A_OUTPUT].channels = 0;
 					outputs[RETRIG_B_OUTPUT].channels = channels;
 					outputs[RETRIG_B_OUTPUT].setVoltageSimd(inputs[RETRIG_INPUT].getPolyVoltageSimd<float_4>(c), c);
@@ -190,6 +230,14 @@ struct SplitStack : Module {
 					outputs[GATE_A_OUTPUT].setVoltageSimd(inputs[GATE_INPUT].getPolyVoltageSimd<float_4>(c), c);
 					outputs[GATE_B_OUTPUT].channels = 0;
 
+					outputs[VEL_A_OUTPUT].channels = channels;
+					outputs[VEL_A_OUTPUT].setVoltageSimd(inputs[VEL_INPUT].getPolyVoltageSimd<float_4>(c), c);
+					outputs[VEL_B_OUTPUT].channels = 0;
+
+					outputs[AFT_A_OUTPUT].channels = channels;
+					outputs[AFT_A_OUTPUT].setVoltageSimd(inputs[AFT_INPUT].getPolyVoltageSimd<float_4>(c), c);
+					outputs[AFT_B_OUTPUT].channels = 0;
+
 					outputs[RETRIG_A_OUTPUT].channels = channels;
 					outputs[RETRIG_A_OUTPUT].setVoltageSimd(inputs[RETRIG_INPUT].getPolyVoltageSimd<float_4>(c), c);
 					outputs[RETRIG_B_OUTPUT].channels = 0;
@@ -203,27 +251,34 @@ struct SplitStack : Module {
 struct SplitStackWidget : ModuleWidget {
 	SplitStackWidget(SplitStack* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/SplitStack.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/SplitStack.svg")), createPanel(asset::plugin(pluginInstance, "res/SplitStack-dark.svg")));
 
 		addChild(createWidget<ThemedScrew>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ThemedScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ThemedScrew>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ThemedScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(22.86, 19.237)), module, SplitStack::STACK_PARAM, SplitStack::STACK_LIGHT));
-		addParam(createLightParamCentered<VCVLightButton<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(22.86, 35.3)), module, SplitStack::SPLIT_PARAM, SplitStack::SPLIT_LIGHT));
-		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(22.86, 51.363)), module, SplitStack::SWITCH_PARAM, SplitStack::SWITCH_LIGHT));
+		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(11.43, 16.062)), module, SplitStack::STACK_PARAM, SplitStack::STACK_LIGHT));
+		addParam(createLightParamCentered<VCVLightButton<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(34.29, 16.062)), module, SplitStack::SPLIT_PARAM, SplitStack::SPLIT_LIGHT));
+		addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(11.43, 28.95)), module, SplitStack::SWITCH_PARAM, SplitStack::SWITCH_LIGHT));
 
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 78.196)), module, SplitStack::VOCT_INPUT));
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 94.258)), module, SplitStack::GATE_INPUT));
-		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 110.321)), module, SplitStack::RETRIG_INPUT));
+		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 51.456)), module, SplitStack::VOCT_INPUT));
+		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 66.46)), module, SplitStack::GATE_INPUT));
+		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 81.465)), module, SplitStack::VEL_INPUT));
+		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 96.469)), module, SplitStack::AFT_INPUT));
+		addInput(createInputCentered<ThemedPJ301MPort>(mm2px(Vec(7.62, 111.473)), module, SplitStack::RETRIG_INPUT));
 
-		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(22.86, 78.196)), module, SplitStack::VOCT_A_OUTPUT));
-		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(38.1, 78.196)), module, SplitStack::VOCT_B_OUTPUT));
-		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(22.86, 94.258)), module, SplitStack::GATE_A_OUTPUT));
-		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(38.1, 94.258)), module, SplitStack::GATE_B_OUTPUT));
-		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(22.86, 110.321)), module, SplitStack::RETRIG_A_OUTPUT));
-		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(38.1, 110.321)), module, SplitStack::RETRIG_B_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(22.86, 51.456)), module, SplitStack::VOCT_A_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(38.1, 51.456)), module, SplitStack::VOCT_B_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(22.86, 66.46)), module, SplitStack::GATE_A_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(38.1, 66.46)), module, SplitStack::GATE_B_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(22.86, 81.465)), module, SplitStack::VEL_A_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(38.1, 81.465)), module, SplitStack::VEL_B_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(22.86, 96.469)), module, SplitStack::AFT_A_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(38.1, 96.469)), module, SplitStack::AFT_B_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(22.86, 111.473)), module, SplitStack::RETRIG_A_OUTPUT));
+		addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(38.1, 111.473)), module, SplitStack::RETRIG_B_OUTPUT));
+
 	}
 };
 
